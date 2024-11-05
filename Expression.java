b5df4bf7a09cb0762a90cd9022a715fdbdeeec6e
Selma Mataiche <mataiche.selma77@gmail.com>
2024-11-05 12:20:46 +0100

    Update Expression.java

--- Expression.java
+++ Expression.java
@@ -1,4 +1,17 @@
+/**
+ * Classe Abstraite Expression
+ */
+
 public abstract class Expression{
+/**
+    *Cette fonction permet d'accéder à la valeur d'une expression.
+    *
+    *Elle doit être implémenter dans les classes fille de Expression.
+    *
+    * Elle retourne un nombre décimal.
+    *
+    *@return un double 
+ */
     
     public abstract double valeur();
 } 
