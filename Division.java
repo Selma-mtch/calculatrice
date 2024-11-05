16dcd0887fd7f032f8132a79ca8240f441db4e26
Selma Mataiche <mataiche.selma77@gmail.com>
2024-11-05 12:31:39 +0100

    Update Division.java

--- Division.java
+++ Division.java
@@ -1,27 +1,42 @@
+/**
+ * Classe Division
+ */
 public class Division extends Operation
 {
+
+/**
+ * Constructeur crée une division composée de deux Expressions placées en paramètres.
+ *  @param op1 : une Expression
+ * @param op2: une Expression
+ */
+
     public Division(Expression op1, Expression op2)
     {
         super(op1,op2);
     }
 
-    public double valeur() throws ArithmeticException
+/**
+ * Cette fonction renvoie le résultat de la division.
+ * @return double : le résultat de la division courante.
+ * @throws ArithmeticException If an input or output exception occurred
+ */
+    public double valeur()
     {
         try{
             double operande1= this.getOperande1().valeur();
-            double operande2= this.getOperande2().valeur();
-            
-            if (operande2==0){
-                throw new ArithmeticException("Division par zéro impossible!");
-            }
-           return operande1/operande2;
+            double operande2= this.getOperande2().valeur(); 
+            return this.getOperande1().valeur() / this.getOperande2().valeur();
         }
 
         catch(ArithmeticException e) {
-            System.err.println(e.getMessage());
-            throw e;
+            System.out.println(e.getMessage());
         }
     }
+
+/**
+ * Cette fonction toString renvoie descritpion de la division sans le résultat.
+ *  @return String: la division
+ */
 
     public String toString()
     {
