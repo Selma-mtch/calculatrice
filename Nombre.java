d8caad22be07e24995bd2e21e1da7c651117f3a8
Selma Mataiche <mataiche.selma77@gmail.com>
2024-11-05 12:21:50 +0100

    Update Nombre.java

--- Nombre.java
+++ Nombre.java
@@ -1,6 +1,14 @@
+/**
+ * Classe Nombre
+ */
 public class Nombre extends Expression
 {
     private double valeurNombre;
+
+/**
+ * Ce constructeur créer un nombre ayant pour valeur celle placée en paramètre. 
+ * @param valeur : un nombre décimal
+ */
 
     //constructeur champs à champs
     public Nombre(double valeur)
@@ -8,12 +16,21 @@ public class Nombre extends Expression
         this.valeurNombre=valeur ;
     }
 
+/**
+ * Cette fonction renvoie la valeur du nombre.
+ *  @return double : la valeur de l'objet courant.
+ */
+
     //renvoie la valeur du nombre
     public double valeur()
     {
         return this.valeurNombre;
     }
 
+/**
+ * Cette fonciton toString renvoie la valeur de l'objet courant dans une chaîne de caractère.
+ * @return String 
+ */
     //description du nombre
     public String toString()
     { 
